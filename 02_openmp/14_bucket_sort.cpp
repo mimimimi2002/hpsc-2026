92a00d43e03c8b20a95e41eae0e55a23e213649e
mimimimi2002 <mimimimi2002.ma@gmail.com>
2026-04-23 23:20:48 +0900

    add parallel to 14

--- 02_openmp/14_bucket_sort.cpp
+++ 02_openmp/14_bucket_sort.cpp
@@ -6,6 +6,7 @@ int main() {
   int n = 50;
   int range = 5;
   std::vector<int> key(n);
+  #pragma omp for
   for (int i=0; i<n; i++) {
     key[i] = rand() % range;
     printf("%d ",key[i]);
@@ -13,11 +14,13 @@ int main() {
   printf("\n");
 
   std::vector<int> bucket(range,0); 
+  #pragma omp for
   for (int i=0; i<n; i++)
     bucket[key[i]]++;
   std::vector<int> offset(range,0);
   for (int i=1; i<range; i++) 
     offset[i] = offset[i-1] + bucket[i-1];
+  #pragma omp parallel
   for (int i=0; i<range; i++) {
     int j = offset[i];
     for (; bucket[i]>0; bucket[i]--) {
